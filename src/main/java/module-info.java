9c17bddccf750859f71270659e6c4b8b686fadd8
nrodr <nrodriguesalves05@gmail.com>
2025-03-26 02:09:44 -0300

    fix: update folder paths in 'module-info.java'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,8 @@ module br.com.joaocarloslima {
     requires javafx.fxml;
 
     opens br.com.joaocarloslima to javafx.fxml;
+    exports br.com.joaocarloslima.controller;
+    opens br.com.joaocarloslima.controller to javafx.fxml;
+
     exports br.com.joaocarloslima;
 }
